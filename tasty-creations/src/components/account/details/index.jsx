c3a147ae2b45bc280392a35acaf6fe229a9db3a0
Tahsin Rahman <trahman31@myseneca.ca>
2022-10-05 23:33:22 -0400

    add account details page

--- tasty-creations/src/components/account/details/index.jsx
+++ tasty-creations/src/components/account/details/index.jsx
@@ -1,3 +1,31 @@
-export default () => {
-    return <h2>Account Details</h2>
-}
+import { Link } from "react-router-dom";
+import "./style.css";
+
+export default () => (
+  <div>
+    <h1
+      id="page-title"
+      style={{
+        textAlign: "center",
+        margin: "2rem 0 2rem",
+      }}
+    >
+      User Account
+    </h1>
+    <p>
+      <h4>Full name</h4>
+      John Doe
+    </p>
+    <p>
+      <h4>E-mail Address</h4>
+      mail@example.com
+    </p>
+    <p>
+      <h4>Gender</h4>
+      Male
+    </p>
+    <p>
+      <Link to="/account/edit">Change information</Link>
+    </p>
+  </div>
+);
