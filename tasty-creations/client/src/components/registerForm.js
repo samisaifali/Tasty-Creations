dca14b5ef9616f73425591bec8a4fcfc58e08bb4
samisaifali <sali263@myseneca.ca>
2022-10-18 22:57:10 -0400

    updated validation

--- tasty-creations/client/src/components/registerForm.js
+++ tasty-creations/client/src/components/registerForm.js
@@ -1,50 +1,83 @@
-import React, {useState, useEffect} from 'react';
-import GoogleLogin from 'react-google-login';
-import "./registerForm.css"
+import React, { useState, useEffect } from 'react'
+import GoogleLogin from 'react-google-login'
+import './registerForm.css'
 
 const RegisterForm = () => {
-    
     useEffect(() => {
         // TODO
     })
-    
 
-    const [popupStyle, showPopup] = useState("hide"); 
-    const popup = () => {
-        showPopup("register-popup")
-        setTimeout(() => showPopup("hide"), 3000);
+    const [popupStyle, showPopup] = useState('hide')
+    const handleSubmit = (e) => {
+        // showPopup('register-popup')
+        // setTimeout(() => showPopup('hide'), 3000)
     }
 
     const onSuccess = (e) => {
-        alert("Account created successfully!");
-        console.log(e);
+        alert('Account created successfully!')
+        console.log(e)
     }
-    
+
     const onFailure = (e) => {
-        alert("User account NOT created!");
-        console.log(e);
+        alert('User account NOT created!')
+        console.log(e)
     }
 
     return (
-        <div className="register">
+        <form onSubmit={handleSubmit} className='register'>
             <h1>Register</h1>
-            <input type="text" placeholder="First name" />
-            <input type="text" placeholder="Last name" />
-            <input type="email" placeholder="Email address" />
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Confirm password" />
-            <div className="register-button" onClick={popup}> Register</div>
-            <p classname="text">Register Using</p>
-            <div className="alter-register">
-                <div className="google">
-                    <GoogleLogin classname="google-register" 
-                        clientId="408408598288-o22i4f2u60ggm1pf5aa9is1bctpi75ic.apps.googleusercontent.com" 
-                        buttonText="" 
-                        onSuccess={onSuccess} 
-                        onFailure={onFailure} 
-                        cookiePolicy={'single_host_origin'} 
+            <input
+                type='text'
+                placeholder='First name'
+                pattern='^[A-Za-z]{2,20}$'
+                required
+            />
+
+            <input
+                type='text'
+                placeholder='Last name'
+                pattern='^[A-Za-z]{2,20}$'
+                required
+            />
+
+            <input type='email' placeholder='Email address' required />
+
+            <input
+                type='password'
+                placeholder='Password'
+                maxLength='20'
+                minLength='6'
+                required
+            />
+
+            <input
+                type='password'
+                placeholder='Confirm password'
+                maxLength='20'
+                minLength='6'
+                required
+            />
+
+            <button
+                style={{ border: 'none' }}
+                className='register-button'
+                type='submit'
+            >
+                Register
+            </button>
+
+            <p classname='text'>Register Using</p>
+            <div className='alter-register'>
+                <div className='google'>
+                    <GoogleLogin
+                        classname='google-register'
+                        clientId='408408598288-o22i4f2u60ggm1pf5aa9is1bctpi75ic.apps.googleusercontent.com'
+                        buttonText=''
+                        onSuccess={onSuccess}
+                        onFailure={onFailure}
+                        cookiePolicy={'single_host_origin'}
                         isSignedIn={false} //we can change the value here to "true", which will keep the sign in status
-                        icon={false} 
+                        icon={false}
                         theme='dark'
                     />
                 </div>
@@ -56,8 +89,8 @@ const RegisterForm = () => {
                 <h3>Registration Failed</h3>
                 <p>All fields are required.</p>
             </div>
-        </div>
+        </form>
     )
 }
 
-export default RegisterForm;
+export default RegisterForm
